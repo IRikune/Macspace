26dd77f165b9dc74e5b8c88dccf9e760ae3ac8d8
CodeArmer <geanmotas@gmail.com>
2025-03-10 09:03:23 -0400

    Added redirect to protect buy route

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,12 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+
+  redirects: {
+    'buy': '/',
+  },
   vite: {
-      plugins: [tailwindcss()],
+    plugins: [tailwindcss()],
   },
 
   integrations: [preact()],
